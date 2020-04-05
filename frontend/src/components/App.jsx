6afebc12f0f5023f2f0d6b30c45a3291e5ccc02d
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-04 23:11:31 -0700

    Use material-ui CSS reset

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import "../App.css";
-import Routes from "./Routes.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import NavBar from "./NavBar.jsx";
+import Routes from "./Routes.jsx";
 
 const App = () => {
   return (
     <Router>
+      <CssBaseline />
       <NavBar />
       <Routes />
     </Router>
