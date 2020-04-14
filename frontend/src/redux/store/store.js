b5339c7e68ba306060b69729ff224255ee6bafee
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-14 14:46:25 -0700

    Added redux-thunk support

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,8 +1,6 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import rootReducer from "../reducers/reducers";
 
-export default createStore(
-  rootReducer,
-  undefined,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+export default createStore(rootReducer, applyMiddleware(thunk));
