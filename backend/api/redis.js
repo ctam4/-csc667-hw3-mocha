ff05358bd10fd86022a03b8c9d1db91fe97913ac
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-13 22:41:05 -0700

    Use async-redis instead

--- backend/api/redis.js
+++ backend/api/redis.js
@@ -1,6 +1,6 @@
-const redis = require('redis');
+const asyncRedis = require('async-redis');
 const redisOptions = {
   host: process.env.REDIS_HOST || '127.0.0.1',
 };
 
-module.exports = redis.createClient(redisOptions);
+module.exports = asyncRedis.createClient(redisOptions);
