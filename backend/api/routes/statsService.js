608d859409c0ae8a23c520c34c36993a6b11d58f
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-13 22:47:27 -0700

    Make redis calls await

--- backend/api/routes/statsService.js
+++ backend/api/routes/statsService.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const redis = require('../redis.js');
 
-router.get('/get', (req, res) => {
-  console.log(redis);
-  redis.get('calls', (err, value) => {
-    if (err) {
-      console.log(err);
-      res.sendStatus(500).end();
-    }
-    if (value === null) {
-      value = 0;
-    }
-    res.send(`The current value of calls is ${value}.`);
-  });
+router.get('/get', async (req, res) => {
+  await redis.get('calls')
+             .then((reply) => {
+               if (reply === null) {
+                 reply = 0;
+               }
+               res.send(`The current value of 'calls' is ${reply}.`);
+             })
+             .catch((err) => {
+               console.log(err);
+               res.sendStatus(500).end();
+             });
 });
 
 module.exports = router;
