674e39f84e096b0412f895a585dd7020bd6c0567
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-13 23:23:41 -0700

    Code reformat

--- backend/api/routes/statsService.js
+++ backend/api/routes/statsService.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const redis = require('../redis.js');
 
 router.get('/get', async (req, res) => {
-  await redis.get('calls')
-             .then((reply) => {
-               if (reply === null) {
-                 reply = 0;
-               }
-               res.send(`The current value of 'calls' is ${reply}.`);
-             })
-             .catch((err) => {
-               console.log(err);
-               res.sendStatus(500).end();
-             });
+  await redis
+  .get('calls')
+  .then((reply) => {
+    if (reply === null) {
+      reply = 0;
+    }
+    res.send(`The current value of 'calls' is ${reply}.`);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.sendStatus(500).end();
+  });
 });
 
 module.exports = router;
