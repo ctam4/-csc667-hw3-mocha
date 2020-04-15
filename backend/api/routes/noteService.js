a25d49b63c1be6240429a02fde08d0573a052d2b
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-14 20:59:25 -0700

    Fix noteService to use new authService endpoints

--- backend/api/routes/noteService.js
+++ backend/api/routes/noteService.js
@@ -10,8 +10,6 @@ if (process.env.GATEWAY_HTTP_PORT) {
 router.use(async (req, res, next) => {
   const params = req.body;
   let authenticated = false;
-  authenticated = true; //debug
-  /*
   if (params.hasOwnProperty('token') && params.token.length > 0) {
     await fetch('http://' + gatewayUrl + '/auth/authenticate', {
       method: 'post',
@@ -34,7 +32,6 @@ router.use(async (req, res, next) => {
     })
     .catch(console.log);
   }
-  */
   if (!authenticated) {
     res.sendStatus(401).end();
   }
@@ -45,10 +42,13 @@ router.post('/get', async (req, res) => {
   const params = req.body;
   let status, response;
   // validate params
-  if (Object.keys(params).length == 2 && params.hasOwnProperty('user') && params.user.length > 0) {
+  if (Object.keys(params).length == 1) {
+    // decode base64 token to two items
+    let decoded = atob(params.token).split(":");
+    decoded[0] = decoded[0].toLowerCase;
     // retrieve from redis
     await redis
-    .lrange(params.user, 0, -1)
+    .lrange(decoded[0], 0, -1)
     .then((reply) => {
       status = 'OK';
       response = JSON.stringify(reply);
@@ -77,10 +77,13 @@ router.post('/create', async (req, res) => {
   const params = req.body;
   let status, response;
   // validate params
-  if (Object.keys(params).length == 3 && params.hasOwnProperty('user') && params.user.length > 0 && params.hasOwnProperty('notesinput') && params.notesinput.length > 0) {
+  if (Object.keys(params).length == 2 && params.hasOwnProperty('notesinput') && params.notesinput.length > 0) {
+    // decode base64 token to two items
+    let decoded = atob(params.token).split(":");
+    decoded[0] = decoded[0].toLowerCase;
     // send to redis
     await redis
-    .lpush(params.user, params.notesinput)
+    .lpush(decoded[0], params.notesinput)
     .then((reply) => {
       status = 'OK';
       response = JSON.stringify(reply);
