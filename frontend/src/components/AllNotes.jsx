212765c9d8ff77fa8cd268659ae9664410af96ac
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-04-18 14:42:52 -0700

    Add AllNotes' useEffect and fetch call

--- frontend/src/components/AllNotes.jsx
+++ frontend/src/components/AllNotes.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useCookies } from "react-cookie";
 import { makeStyles } from "@material-ui/core/styles";
 import { ListItem, IconButton, ListItemText, ListItemSecondaryAction } from "@material-ui/core";
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 
+import { apiUrl } from "../url";
 import { removeNote } from "../redux/actions/actions";
 
 const useStyles = makeStyles(theme => ({
@@ -12,8 +14,37 @@ const useStyles = makeStyles(theme => ({
 
 export const AllNotes = () => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => state.notes);
+  const [cookies, setCookie] = useCookies(["token"]);
+  let notes = [];
   const classes = useStyles();
+
+  useEffect(async () => {
+    await fetch(apiUrl + '/notes/get', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: cookies.token,
+      }),
+    })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('error ' + res.status);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      if (data.status === 'ERROR') {
+        throw new Error(data.response);
+      }
+      else {
+        notes = data.response;
+      }
+    })
+    .catch(alert);
+  });
 
   return (
     <React.Fragment>
